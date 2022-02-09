60bc111237836f64011d53cf53d882bdd9609910
fenilgholani <fgholani@umd.edu>
2022-02-09 17:28:12 -0500

    Added login page

---
+++ app/src/main/java/com/example/shoeping/LoginActivity.kt
@@ -0,0 +1,13 @@
+package com.example.shoeping
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+
+    }
+}
