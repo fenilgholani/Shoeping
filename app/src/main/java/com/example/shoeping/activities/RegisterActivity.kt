6dfbb3af36a4559cb3bc866f95e9a08f16b41e02
fenilgholani <fgholani@umd.edu>
2022-02-21 10:19:14 -0500

    Created Register but WIP

--- app/src/main/java/com/example/shoeping/activities/RegisterActivity.kt
+++ app/src/main/java/com/example/shoeping/activities/RegisterActivity.kt
@@ -3,18 +3,19 @@ package com.example.shoeping.activities
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
 import android.widget.TextView
 import com.example.shoeping.R
 
-class LoginActivity : AppCompatActivity() {
+class RegisterActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        setContentView(R.layout.activity_register)
 
-        findViewById<TextView>(R.id.tv_register).setOnClickListener {
-            val intent = Intent(this@LoginActivity, RegisterActivity::class.java)
+        findViewById<TextView>(R.id.tv_register_login).setOnClickListener {
+            val intent = Intent(this, LoginActivity::class.java)
             startActivity(intent)
         }
+
+
     }
 }
